113566a25e5c3de1eb60832e8114c6be20880487
ChoiByeol <honey333888@naver.com>
2021-01-15 14:24:27 +0900

    URL로 입력된 값 사용하기(쿼리스트링 해석)

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(req, res) {
-    var url = req.url;
-    if(req.url == '/'){
-        url = '/index.html';
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+
+    if(_url == '/'){
+        _url = '/index.html';
     }
-    if(req.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         return res.writeHead(404);
-        Response.end();
-        return;
     }
     res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + url));
+    res.end(queryData.id);
+
 });
 app.listen(3000);
